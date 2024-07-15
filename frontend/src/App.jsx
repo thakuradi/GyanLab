865f260ca9bfa03ba01101de1c13e5d6d38aebbb
Aditya Kumar <84377163+thakuradi@users.noreply.github.com>
2024-07-15 20:13:18 +0530

    question component added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,23 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+import { Input } from "./components/ui/input";
+import { useState } from "react";
 
 export default function Home() {
+  const [val, setVal] = useState("");
+
+  function reset() {
+    setVal("");
+  }
+
   return (
-    <div>
-      <div>hello world</div>
-      <Button>Click me</Button>
+    <div className="m-10">
+      <Input
+        placeholder=""
+        className="h-20 max-w-[800px]"
+        value={val}
+        onChange={(e) => setVal(e.target.value)}
+      />
+      <Button onClick={reset}>click me</Button>
     </div>
-  )
+  );
 }
