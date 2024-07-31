50a4dd68d56d8e04d98160156f2498362cd77752
thakuradi <aditya25022002@gmail.com>
2024-07-31 14:31:44 +0530

    a

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
-// mongodb+srv://thakur:7X%403uw8r@cluster0.ai2rpio.mongodb.net/
+//mongodb+srv://aditya25022002:PaC7BiTBkI4EnliG@gyanlab.jpxqkvy.mongodb.net/
 mongoose.connect(
-  "mongodb+srv://aditya25022002:rOzA619i71BJMpq3@gyanlab.jpxqkvy.mongodb.net/"
+  "mongodb+srv://aditya25022002:PaC7BiTBkI4EnliG@gyanlab.jpxqkvy.mongodb.net/"
 );
 
 const QnAnSchema=mongoose.Schema({
@@ -18,8 +18,6 @@ const UserSchema = mongoose.Schema({
 const qnans=mongoose.model("qnans",QnAnSchema)
 const User = mongoose.model("User", UserSchema);
 module.exports = {
-  question: question,
-  answer: answer,
   User: User,
   qnans:qnans,
 };
