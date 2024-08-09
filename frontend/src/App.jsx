70ee213ab17ca690ca5f55f2fb365244f3fc07c3
amrutha <amruthabeedikar@gmail.com>
2024-08-09 21:57:26 +0530

    Home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,13 @@ import { Signup } from "./Pages/signup";
 import { Signin } from "./Pages/signin";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Ans } from "./Pages/Ans";
+import { HomePage } from "./Pages/homePage";
 export default function Home() {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={< HomePage/>}></Route>
           <Route path="/signin" element={<Signin />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/qnans" element={<QnAns />} />
