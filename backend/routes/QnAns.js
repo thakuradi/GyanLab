2e3d9d1770d8a5b01bfea7e6abf1f1b6a6ab4fba
thakuradi <aditya25022002@gmail.com>
2024-08-13 15:03:49 +0530

    adding upload photos in qnans

--- backend/routes/QnAns.js
+++ backend/routes/QnAns.js
@@ -4,10 +4,30 @@ const { qnans, answer,images} = require("../db");
 const { authMiddleware } = require("../middleware/middleware");
 const app = express.Router();
 const multer = require("multer");
+const path = require('path');
 const zod=require("zod");
 app.use(express.json());
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname))
+  }
+})
 
-app.post("/questions", authMiddleware, async function (req, res) {
+const upload = multer({ 
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype.startsWith('image/')) {
+      cb(null, true);
+    } else {
+      cb(new Error('Not an image! Please upload an image.'), false);
+    }
+  }
+})
+app.post("/questions",upload.single("image"),authMiddleware, async function (req, res) {
   const createPayload = req.body;
   const parsePayload = Createqn.safeParse(createPayload);
   if (!parsePayload.success) {
@@ -16,9 +36,11 @@ app.post("/questions", authMiddleware, async function (req, res) {
     });
     return;
   }
+  console.log(req.body)
   await qnans.create({
     userID: req.userId,
     question: req.body.question,
+    image: req.file ? req.file.filename : null
   });
   res.json({
     msg: "question added",
@@ -32,17 +54,7 @@ app.get("/userquestion", authMiddleware, async function (req, res) {
     question
   });
 });
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/')
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() 
-    cb(null, uniqueSuffix=file.originalname)
-  }
-})
 
-const upload = multer({ storage: storage })
 app.post("/upload",upload.single("image"),async function(req,res){
   const imagename=req.file.filename  
   try {
--- frontend/src/Pages/QnAns.jsx
+++ frontend/src/Pages/QnAns.jsx
@@ -4,8 +4,10 @@ import { Label } from '@/components/ui/label';
 import { DisplayQnANs } from '@/components/ui/displayquestion';
 import { AddingQuestion } from '@/components/ui/qnadd';
 import { Button } from '@/components/ui/button';
+import Navbar from '@/components/ui/navbar';
   export function QnAns() {
  return <div>
+  <Navbar></Navbar>
   <AddingQuestion></AddingQuestion>
   <DisplayQnANs></DisplayQnANs>
  </div>
